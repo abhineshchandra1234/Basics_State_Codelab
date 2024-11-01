820e04bdab83d8b7502708363a6c022bb3e0045a
abhinesh chandra <abhineshchandra1234@gmail.com>
2024-11-01 11:01:18 +0530

    not able to check tasks

--- app/src/main/java/com/example/basicsstatecodelab/WellnessTaskList.kt
+++ app/src/main/java/com/example/basicsstatecodelab/WellnessTaskList.kt
@@ -9,6 +9,7 @@ import androidx.compose.ui.Modifier
 @Composable
 fun WellnessTasksList(
     list: List<WellnessTask>,
+    onCheckedTask: (WellnessTask, Boolean) -> Unit,
     onCloseTask: (WellnessTask) -> Unit,
     modifier: Modifier = Modifier
 ) {
@@ -20,6 +21,8 @@ fun WellnessTasksList(
             key = { task -> task.id }
         ) { task ->
             WellnessTaskItem(taskName = task.label,
+                checked = task.checked,
+                onCheckedChange = { checked -> onCheckedTask(task, checked) },
                 onClose = { onCloseTask(task) })
         }
     }
